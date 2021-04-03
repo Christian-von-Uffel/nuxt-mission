7f2053912248ad54b09078644c4601e0a8c1a239
Christian von Uffel <christian.uffel@gmail.com>
2021-04-03 16:02:00 -0400

    fetch planets data

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -7,3 +7,11 @@
     </div>
 </template>
 
+<script>
+export default {
+    async asyncData() {
+        const planet = await fetch("https://api.nuxtjs.dev/planets/mars").then((res) => res.json())
+        return { planet }
+    }
+}
+</script>
