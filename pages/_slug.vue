fcfa333d52292288de2f2fe47c22fcea8dc19507
Christian von Uffel <christian.uffel@gmail.com>
2021-04-03 16:28:10 -0400

    style the page

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        {{ $route.params.slug }}
-        <pre>
+    <div class="mx-auto px-4 py-16">
+        <img :src="planet.image" :alt="planet.title" class="max-w-sm rounded-lg shadow-lg overflow-hidden mx-auto">
+        <h1 class="text-center text-3xl font-bold mt-12 mb-12">
+        {{ planet.title }}
+        </h1>
+        <pre class="flex flex-wrap max-w-screen-md mx-auto">
             {{$data}}
         </pre>
     </div>
